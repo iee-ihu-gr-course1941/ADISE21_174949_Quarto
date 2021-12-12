b470773479749066cae5c61fc65d1f130dd15255
lhz <pepitsizous@zoho.com>
2021-12-12 15:25:55 +0200

    update testing script

--- invite_and_join_flow.sh
+++ invite_and_join_flow.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
+#!/bin/sh -x
 
-#go run main.go vars.go
+# base URL
+#BASE_URL="localhost:8000"
+BASE_URL="https://users.iee.ihu.gr/~it174949/index.php"
 
 # make user, get uid json
 U=$(curl -s -X POST\
 	-H "Content-Type: application/json"\
 	-d '{"username": "user", "password": "verybigsecret"}'\
-	localhost:8000/user)
+	${BASE_URL}/user)
 
 # make game, get raw gid
-GID=$(curl -s -X POST -H "Content-Type: application/json" -d "$U" localhost:8000/game | jq -r '.game_id')
+GID=$(curl -s -X POST -H "Content-Type: application/json" -d "${U}" ${BASE_URL}/game | jq -r '.game_id')
 
 # print both
 echo $U
 echo $GID
 
-LNK="localhost:8000/game/${GID}"
+LNK="${BASE_URL}/game/${GID}"
 
 G=$(curl -s "${LNK}")
 
@@ -25,25 +27,25 @@ echo $G
 U2=$(curl -s -X POST\
 	-H "Content-Type: application/json"\
 	-d '{"username": "u2", "password": "hugesecret"}'\
-	localhost:8000/user)
+	${BASE_URL}/user)
 
 U2UN=$(echo ${U2} | jq -r '.username')
 
 LNK="${LNK}/invite/${U2UN}"
 
-INV=$(curl -s -X POST -H "Content-Type: application/json" -d "$U" "$LNK")
+INV=$(curl -s -X POST -H "Content-Type: application/json" -d "${U}" "${LNK}")
 
-LNK="localhost:8000/game/${GID}"
+LNK="${BASE_URL}/game/${GID}"
 
 G=$(curl -s "${LNK}")
 
 #change link to join
-LNK="localhost:8000/game/${GID}/join"
+LNK="${BASE_URL}/game/${GID}/join"
 
-JOIN_RES1=$(curl -s -X POST -H "Content-Type: application/json" -d "$U" "$LNK")
+JOIN_RES1=$(curl -s -X POST -H "Content-Type: application/json" -d "${U}" "${LNK}")
 
 echo $JOIN_RES1
 
-JOIN_RES2=$(curl -s -X POST -H "Content-Type: application/json" -d "$U2" "$LNK")
+JOIN_RES2=$(curl -s -X POST -H "Content-Type: application/json" -d "${U2}" "${LNK}")
 
 echo $JOIN_RES2
