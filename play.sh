57f8a6fb3597cb52306887082f5d4172831304b8
lhz <pepitsizous@zoho.com>
2022-01-15 17:54:14 +0200

    add second round to play.sh

--- play.sh
+++ play.sh
@@ -55,6 +55,7 @@ JOIN_RES2=$(curl -s -X POST -H "Content-Type: application/json" -d "${U2}" "${LN
 #change link to play
 LNK="${BASE_URL}/game/${GID}/play"
 
+#user 1 play 1
 PLAY_DATA='{"username":"user","user_id":"U1UID", "position_x":3, "position_y":2, "next_piece":{"Id":2,"Dark":true,"Short":false,"Hollow":false,"Round":false}}'
 
 PLAY_DATA=$(echo $PLAY_DATA | sed s/U1UID/${U1UID}/g)
@@ -64,6 +65,7 @@ PLAY_RES=$(curl -s -X POST\
 	-d "${PLAY_DATA}"\
 	"${LNK}")
 
+#user 2 play 1
 PLAY_DATA='{"username":"u2","user_id":"U2UID", "position_x":1, "position_y":0, "next_piece":{"Id":4,"Dark":false,"Short":false,"Hollow":false,"Round":true}}'
 
 PLAY_DATA=$(echo $PLAY_DATA | sed s/U2UID/${U2UID}/g)
@@ -72,3 +74,24 @@ PLAY_RES=$(curl -s -X POST\
 	-H 'Content-Type: application/json'\
 	-d "${PLAY_DATA}"\
 	"${LNK}")
+
+#user 1 play 2
+PLAY_DATA='{"username":"user","user_id":"U1UID", "position_x":0, "position_y":2, "next_piece":{"Id":11,"Dark":false,"Short":true,"Hollow":true,"Round":true}}'
+
+PLAY_DATA=$(echo $PLAY_DATA | sed s/U1UID/${U1UID}/g)
+
+PLAY_RES=$(curl -s -X POST\
+	-H 'Content-Type: application/json'\
+	-d "${PLAY_DATA}"\
+	"${LNK}")
+
+#user 2 play 2
+PLAY_DATA='{"username":"u2","user_id":"U2UID", "position_x":2, "position_y":1, "next_piece":{"Id":14,"Dark":true,"Short":true,"Hollow":true,"Round":false}}'
+
+PLAY_DATA=$(echo $PLAY_DATA | sed s/U2UID/${U2UID}/g)
+
+PLAY_RES=$(curl -s -X POST\
+	-H 'Content-Type: application/json'\
+	-d "${PLAY_DATA}"\
+	"${LNK}")
+
